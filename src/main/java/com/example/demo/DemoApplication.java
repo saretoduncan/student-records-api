41a1a700e26d825e86f74b521df42ceebf7fdc8c
saretoduncan <saretoduncan@gmail.com>
2021-09-12 14:56:08 +0300

    ready for deployment

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import com.example.demo.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+
+public class DemoApplication {
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+	}
+	public static void main(String[] args) {
+		port(getHerokuAssignedPort());
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}

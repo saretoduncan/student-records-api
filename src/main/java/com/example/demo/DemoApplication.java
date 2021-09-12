23d843450fa9872b94989e74740beb8c8a21125d
saretoduncan <saretoduncan@gmail.com>
2021-09-12 14:58:58 +0300

    modifu pom

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,15 +13,9 @@ import java.util.List;
 @SpringBootApplication
 
 public class DemoApplication {
-	static int getHerokuAssignedPort() {
-		ProcessBuilder processBuilder = new ProcessBuilder();
-		if (processBuilder.environment().get("PORT") != null) {
-			return Integer.parseInt(processBuilder.environment().get("PORT"));
-		}
-		return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-	}
+
 	public static void main(String[] args) {
-		port(getHerokuAssignedPort());
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
